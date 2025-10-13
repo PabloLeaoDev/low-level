21ec6a273e9004c4283fdeb4545fed0b9e05f38b
Pablo Leão <151883726+PabloLeaoDev@users.noreply.github.com>
2025-10-13 00:10:19 -0300

    started

---
+++ c-essentials/exercises/gerenciaAlunos.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+typedef struct Aluno {
+  char nome[50];
+  char curso[30];
+  int dataNascimento[3];
+  int matricula;
+  float altura;
+  float peso;
+  float notas[3];
+  float mediaFinal;
+} Aluno;
+
+int main(int argc, char* argv[]) {
+
+}
